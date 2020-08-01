6194d2bc7ea227cb8ee5164f77bdb4289019942e
Kabindra Shrestha <kabindra.shrestha.0wn@gmail.com>
2020-08-01 20:44:27 +0545

    solved warning for list key

--- src/user/list/UserList.js
+++ src/user/list/UserList.js
@@ -11,61 +11,6 @@ const useStyles = theme => ({
         borderWidth: '.2rem',
         position: 'relative'
     },
-    content: {
-        padding: '2rem 2rem !important',
-        backgroundColor: theme.palette.card.background,
-        borderRadius: '.3rem'
-    },
-    title: {
-        fontSize: '3.5rem',
-        fontWeight: 300,
-        lineHeight: 1.2,
-        marginBottom: '.5rem',
-        marginTop: 0,
-        display: 'block',
-        marginBlockStart: '0.67em',
-        marginBlockEnd: '0.67em',
-        marginInlineStart: '0px',
-        marginInlineEnd: '0px',
-        color: theme.palette.text,
-        textAlign: 'left'
-    },
-    quote: {
-        fontSize: '1.25rem',
-        fontWeight: 300,
-        marginTop: 0,
-        marginBottom: '1rem',
-        display: 'block',
-        marginBlockStart: '1em',
-        marginBlockEnd: '1em',
-        marginInlineStart: '0px',
-        marginInlineEnd: '0px',
-        lineHeight: 1.5,
-        color: theme.palette.text,
-        textAlign: 'left'
-    },
-    footer: {
-        marginTop: 0,
-        marginBottom: '1rem',
-        display: 'block',
-        marginBlockStart: '1em',
-        marginBlockEnd: '1em',
-        marginInlineStart: '0px',
-        marginInlineEnd: '0px',
-        fontSize: '1rem',
-        fontWeight: 400,
-        lineHeight: 1.5,
-        color: theme.palette.text,
-        textAlign: 'left'
-    },
-    space: {
-        marginBottom: '1.5rem!important',
-        marginTop: '1.5rem!important',
-        border: 0,
-        borderTop: '1px solid rgba(0,0,0,.1)',
-        boxSizing: 'content-box',
-        height: 0,
-    },
 });
 
 class UserList extends Component {
@@ -74,12 +19,14 @@ class UserList extends Component {
     }
 
     render() {
-        // const {classes} = this.props;
-        // const {userList, userListData} = this.props;
+        const {classes} = this.props;
         const {userListData} = this.props;
 
         return (
-            <div>{userListData && userListData.map((userList) => <li key={userList}>{userList.username} </li>)}</div>
+            <div className={classes.root}>
+                {userListData && userListData.map((userList) => <li
+                    key={userList.username}>{userList.username} </li>)}
+            </div>
         );
     }
 }
